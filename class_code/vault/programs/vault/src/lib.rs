45d3a91d5694718ca831817bec09eb7aa9c164aa
Hemant <hrsmart2150@gmail.com>
2024-05-23 17:12:41 +0530

    Added tests for vault

--- class_code/vault/programs/vault/src/lib.rs
+++ class_code/vault/programs/vault/src/lib.rs
@@ -13,8 +13,12 @@ pub mod vault {
         ctx.accounts.initialize(&ctx.bumps)
     }
 
-    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+    pub fn deposit(ctx: Context<Payment>, amount: u64) -> Result<()> {
         ctx.accounts.deposit(amount)
+    }
+
+    pub fn withdraw(ctx: Context<Payment>, withdraw_amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(withdraw_amount)
     }
 }
 
@@ -48,7 +52,7 @@ impl<'info> Initialize<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Deposit<'info> {
+pub struct Payment<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     #[account(
@@ -65,94 +69,42 @@ pub struct Deposit<'info> {
     pub system_program: Program<'info, System>,
 }
 
-impl<'info> Deposit<'info> {
+impl<'info> Payment<'info> {
     pub fn deposit(&mut self, amount: u64) -> Result<()> {
-        let cpi_program: AccountInfo = self.system_program.to_account_info();
+        let cpi_program = self.system_program.to_account_info();
 
-        let cpi_accounts: Transfer = Transfer {
+        let cpi_accounts = Transfer {
             from: self.user.to_account_info(),
             to: self.vault.to_account_info(),
         };
 
-        let cpi_ctx: CpiContext<Transfer> = CpiContext::new(cpi_program, cpi_accounts);
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
 
-        transfer(cpi_ctx, amount)?;
-
-        Ok(())
+        transfer(cpi_ctx, amount)
     }
-}
 
-impl<'info> Withdraw<'info> {
+    // Withdraws the funds from a PDA. We will need the PDA to sign.
     pub fn withdraw(&mut self, amount: u64) -> Result<()> {
-        let cpi_program: AccountInfo = self.system_program.to_account_info();
+        let cpi_program = self.system_program.to_account_info();
 
-        let cpi_accounts: Transfer = Transfer {
+        let cpi_accounts = Transfer {
             from: self.vault.to_account_info(),
             to: self.user.to_account_info(),
         };
 
-        // Derive PDA 
-        let seeds = &[
-            b"vault",
-            self.vault_state.to_account_info().key.as_ref(),
-            &[self.vault_state.vault_bump],
-        ];
+        let vault_bump = self.vault_state.vault_bump;
+        let vault_state_key = self.vault_state.key();
 
-        let signer_seeds = &[&seeds[..]];
+        let signer_seeds = &["vault".as_bytes(), vault_state_key.as_ref(), &[vault_bump]];
+        let signer_seeds = [&signer_seeds[..]]; // repackage into a slice of slices.
 
-        let cpi_ctx: CpiContext<Transfer> = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+        let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
 
-        transfer(cpi_ctx, amount)?;
-
-        Ok(())
+        transfer(cpi_context, amount)
     }
 }
 
 #[account]
-pub struct VaultState {
-    pub vault_bump: u8,
-    pub state_bump: u8,
-}
-
-#[derive(Accounts)]
-pub struct Close<'info>{
-    #[account(mut)]
-    pub user: Signer<'info>,
-    #[account(
-        mut,
-        seeds = [b"vault", vault_state.key().as_ref()],
-        bump = vault_state.vault_bump,
-    )],
-    pub vault: SystemAccount<'info>,
-    #[account(
-         mut,
-        seeds = [b"state", user.key().as_ref()],
-        bump = vault_state.vault_bump,
-        close = user,
-    )],
-    pub vault_state: Account<'info, VaultState>,
-    pub system_program: Program<'info, System>,
-}
-
-impl <'info> Close<'info>{
-    pub fn close(&mut self) -> Result<()>{
-        let cpi_program = self.system_program.to_account_info();
-        let cpi_account = Transfer{
-            from: self.vault.to_account_info();
-            to: self.user.to_account_info();
-        };
-        let seeds = &[
-            b"vault",
-            self.vault_state.to_account_info().key.as_ref(),
-            &[self.vault_state.vault_bump],
-        ];
-
-        let signer_seeds = &[&seeds[..]];
-
-        let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_account, transfer(cpi_context, self.vault.lamports()));
-    }
-}
-
 pub struct VaultState {
     pub vault_bump: u8,
     pub state_bump: u8,
